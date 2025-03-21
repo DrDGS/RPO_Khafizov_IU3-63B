fd552d2c17ebd2ec46436cd4fe14e8796d06da63
DrDGS <t.khafizov05@gmail.com>
2025-03-21 13:12:08 +0300

    LAB_1

---
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -0,0 +1,43 @@
+package ru.iu3.fclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import ru.iu3.fclient.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'fclient' library on application startup.
+    static {
+        System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int res = initRng();
+        byte[] key = "sample_key".getBytes();
+        byte[] data_enc = encrypt(key, "Encrypted Text".getBytes());
+        byte[] data_dec = decrypt(key, data_enc);
+
+        String result = new String(data_enc) + "\n" + new String(data_dec);
+
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(result);
+    }
+
+    public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+}
